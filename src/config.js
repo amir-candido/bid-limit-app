6becc805bf63ba60f486e54d0e1fb52598bf228a
amir-canteetu <amircanteetu@gmail.com>
2025-07-25 18:50:01 -0600

    baseline

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
   BIDJS_BASE: process.env.BIDJS_BASE_URL,
   API_KEY: process.env.BIDJS_API_KEY,
-  ACTIVE_AUCTIONS: process.env.ACTIVE_AUCTIONS.split(','),  // e.g. "uuid1,uuid2"
+  // ACTIVE_AUCTIONS: process.env.ACTIVE_AUCTIONS.split(','),  // e.g. "uuid1,uuid2"
   PORT: process.env.PORT || 3000,
   API_SECRET: process.env.API_SECRET,
   Client_ID: process.env.Client_ID,
