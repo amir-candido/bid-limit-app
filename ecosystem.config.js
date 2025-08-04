17dca537dad5b0aef10def8da0a3897f04b5eedd
amir-canteetu <amircanteetu@gmail.com>
2025-08-04 17:46:00 -0600

    added ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  apps: [
+    {
+      name: 'bid-limit-app',
+      script: 'src/index.js',
+      watch: ['src', 'config'],
+      ignore_watch: ['node_modules', 'logs', '*.log',  '*.db'],
+      env_development: {
+        NODE_ENV: 'development',
+        watch: true // Enable watch only in development
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        watch: false // Disable watch in production
+      }
+    }
+  ]
+};
